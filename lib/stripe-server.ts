fc0540a5bc6889b410d985a453ac099c937f0ff2
Jair0305 <chamaco_03@hotmail.es>
2025-03-05 00:32:22 -0600

    fixed: fixed error cant show pago-exitoso page

--- lib/stripe-server.ts
+++ lib/stripe-server.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 
 // Inicialización de Stripe en el lado del servidor
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16', // Usar la versión más reciente disponible
+  apiVersion: '2025-02-24.acacia', // Usar la versión más reciente disponible
   appInfo: {
     name: 'Moonbase Web Services',
     version: '0.1.0',
